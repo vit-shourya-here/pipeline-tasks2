2ab160f96b67ecc9771ea14efdfd9f1b4d2eddda
vit-shourya-here <shourya2024vit@gmail.com>
2026-09-03 07:37:47 +0530

    [BUG01] input()

--- add.py
+++ add.py
@@ -4,7 +4,7 @@ def add(a,b):
     return a+b
 
 if __name__ == "__main__":
-    num1 = int(input(sys.argv[1]))
-    num2 = int(input(sys.argv[2]))
+    num1 = int((sys.argv[1]))
+    num2 = int((sys.argv[2]))
     result = add(num1,num2)
     print("Result = ", result)
